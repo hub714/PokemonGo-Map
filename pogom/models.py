2c9ca2f41ff9eaf9b5862c143e39e0867ca48744
hub714 <hubertcheung@gmail.com>
2016-07-28 01:35:13 +0000

    condition writes to DDB, pull the data from notify.json

--- pogom/models.py
+++ pogom/models.py
@@ -31,6 +31,10 @@ with open('config.json') as data_file:
     json_config = json.load(data_file)
 pprint(json_config)
 
+with open('notify.json') as notify_file:
+    json_notify = json.load(notify_file)
+pprint(json_notify)
+
 dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
 table = dynamodb.Table(json_config['ddb_table'])
 
@@ -240,12 +244,11 @@ def send_to_ddb(encounter_id, pokemon_name, timeleft):
         ddbresponse = table.put_item(
                     Item={
                         'encounter_id': str(encounter_id),
-                        'valid_until': str(timeleft),
+                        'valid_until': int(timeleft),
                         'pokemon_name': str(pokemon_name)
                 },
                 ConditionExpression='attribute_not_exists(encounter_id)'
                 )
-        #pprint(pokemon_name)
         pprint(ddbresponse)
     except:
         log.error("an error") 
@@ -282,8 +285,12 @@ def parse_map(map_dict, iteration_num, step, step_location):
                     'longitude': p['longitude'],
                     'disappear_time': time.mktime(d_t.timetuple())
                 }
-                send_to_ddb(p['encounter_id'], get_pokemon_name(p['pokemon_data']['pokemon_id']), time.mktime(d_t.timetuple()))
-                pprint(p)
+                if str(p['pokemon_data']['pokemon_id']) in json_notify:
+                #pprint(json_notify[str(p['pokemon_data']['pokemon_id'])])
+                    send_to_ddb(p['encounter_id'], get_pokemon_name(p['pokemon_data']['pokemon_id']), time.mktime(d_t.timetuple()))
+                
+                
+                #pprint(p)
                 send_to_webhook('pokemon', webhook_data)
 
         if iteration_num > 0 or step > 50:
